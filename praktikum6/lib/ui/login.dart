f42b235e19c71e3e6936ec0d0f08546335236a5e
fdidan <fdidan09@gmail.com>
2024-06-12 05:36:40 +0700

    Praktikum 9 Firebase Authentication update (kesalahan penamaan project)

---
+++ praktikum6/lib/ui/login.dart
@@ -0,0 +1,194 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:praktikum6/bloc/login/login_cubit.dart';
+import 'package:praktikum6/ui/home_screen.dart';
+import 'package:praktikum6/ui/phone_auth_screen.dart';
+import 'package:praktikum6/utils/routes.dart';
+
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
+  final emailEdc = TextEditingController();
+  final passEdc = TextEditingController();
+
+  bool passInvisible = false;
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential).then(
+      (value) async => await Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+        (route) => false));
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocListener<LoginCubit, LoginState>(
+        listener: (context, state){
+          if(state is LoginSuccess){
+            ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+              content: Text(state.msg),
+              backgroundColor: Colors.green,
+              ));
+            Navigator.pushNamedAndRemoveUntil(context, rHome, (route) => false);
+          }
+          if(state is LoginLoading){
+            ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text('Loading...')));
+          }
+          if(state is LoginFailure){
+            ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+              content: Text(state.msg),
+              backgroundColor: Colors.red,
+              ));
+          }
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 70),
+          child: ListView(
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff3D4DE0)
+                ),
+              ),
+              SizedBox(height: 15,),
+              Text(
+                "Silahkan memasukan e-mail dan password anda",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 25,),
+              Text(
+                "e-mail",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              TextFormField(
+                controller: emailEdc,
+              ),
+              SizedBox(height: 10,),
+              Text(
+                "password",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              TextFormField(
+                controller: passEdc,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    icon: Icon(passInvisible ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        passInvisible = !passInvisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !passInvisible,
+              ),
+              SizedBox(height: 50,),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<LoginCubit>().login(email: emailEdc.text, password: passEdc.text);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff3D4DE0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.white
+                  ),
+                )
+              ),
+              const SizedBox(height: 30.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      signInWithGoogle();
+                    },
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                        'https://banner2.cleanpng.com/20180423/gkw/kisspng-google-logo-logo-logo-5ade7dc753b015.9317679115245306313428.jpg'
+                      )
+                    ),
+                  ),
+                  const SizedBox(width: 30.0,),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PhoneAuthScreen()
+                        )
+                      );
+                    },
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                        'https://banner2.cleanpng.com/20190413/vqq/kisspng-virginia-kahan-orthodontics-la-promise-fund-charte-5cb19cb20086a4.0565734115551438580022.jpg'
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Belum punya akun?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: Text(
+                      "Daftar",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff3D4DE0)
+                      ),
+                    )
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
