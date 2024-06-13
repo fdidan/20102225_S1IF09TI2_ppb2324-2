8352af9f6f3442aa605466b8d36c9b17ef14c940
fdidan <fdidan09@gmail.com>
2024-06-13 16:15:57 +0700

    Pertemuan 3 Menjalankan program Hello World & Contact App

--- Praktikum3/praktikum3/lib/main.dart
+++ Praktikum3/praktikum3/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:praktikum3/bloc/contact_cubit.dart';
+import 'package:praktikum3/views/home_screen.dart';
 
 void main(){
   runApp(const MyApp());
@@ -8,8 +11,13 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   Widget build(BuildContext context){
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ContactCubit())
+      ],
+      child : const MaterialApp(
+        home: HomeScreen(),
+      ),
     );
   }
 }
